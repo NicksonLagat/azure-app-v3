109634b6ba42949d92d5a2aa7f53c5b00eb27ac7
NicksonLagat <vnilag@gmail.com>
2023-03-13 18:06:32 +0300

    Made a slight change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Nickie
+          Learn React Nickie Changed
         </a>
       </header>
     </div>
